220689adbf6d5dedf0ab4bc18f1b7c11d525d7df
Sam Ellenrieder <sam.ell@web.de>
2017-12-27 14:28:11 +0000

    setup

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to my world</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <code>src/App.js</code>
         </p>
       </div>
     );
