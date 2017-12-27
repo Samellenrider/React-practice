58d7e13c08d675cf1d183c3ddb8a6d016feabd4d
Sam Ellenrieder <sam.ell@web.de>
2017-12-27 16:08:17 +0000

    state property

--- src/App.js
+++ src/App.js
@@ -3,16 +3,24 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'Sam', age: 20},
+      {name: 'San', age: 25},
+      {name: 'Emma', age: 20}
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Welcome to my world</h1>
         </header>
-        <Person name="Sam" age="20"> I wrote this stuff </Person>
-        <Person name="San" age="25"> My favorite thing is food! </Person>
-        <Person name="Sam" age="22"> I love Redhead people! </Person>
-        <Person name="Sepp" age="21"> I am a medic student. </Person>
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}> I wrote this stuff </Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> My favorite thing is food! </Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}> I love Zara Larsson! </Person>
       </div>
     );
     ///return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'loololopl'))
