25f9974b0c042da779fb762c34a0fd9cd3cddf0d
Sam Ellenrieder <sam.ell@web.de>
2017-12-27 16:27:21 +0000

    manipulating the state

--- src/App.js
+++ src/App.js
@@ -6,9 +6,20 @@ class App extends Component {
   state = {
     persons: [
       {name: 'Sam', age: 20},
-      {name: 'San', age: 25},
+      {name: 'Chris', age: 25},
       {name: 'Emma', age: 20}
-    ]
+    ],
+    otherState: 'some other thing'
+  }
+
+  switchNameHandler = () => {
+    this.setState({
+      persons: [
+        {name: 'Sam', age: 20},
+        {name: 'Chris', age: 24},
+        {name: 'Emma', age: 20}
+      ] 
+    })
   }
 
   render() {
@@ -17,7 +28,7 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Welcome to my world</h1>
         </header>
-        <button>Switch Name</button>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}> I wrote this stuff </Person>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> My favorite thing is food! </Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}> I love Zara Larsson! </Person>
