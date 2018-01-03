5192b3ef940f9376994feb9dc97d2bba2beffff5
Sam Ellenrieder <sam.ell@web.de>
2018-01-03 16:16:32 +0000

    avoiding unpredictable apps

--- src/App.js
+++ src/App.js
@@ -17,9 +17,10 @@ class App extends Component {
   }
 
   deletePersonHandler = (personIndex) => {
-    const persons = this.state.persons;
+    const persons = this.state.persons.slice();
     persons.splice(personIndex, 1);
     this.setState({persons: persons});
+
   }
   
   togglePersonsHandler = () => {
